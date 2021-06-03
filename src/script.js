b95dc71d7a443d9e7677a92eccb862cfcefb13cb
corienelson <82166983+corienelson@users.noreply.github.com>
2021-06-03 11:31:32 -0700

    take out forecast

--- src/script.js
+++ src/script.js
@@ -18,15 +18,6 @@ function search(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
 
-function getForcast(coordinates) {
-  let apiKey = "f497dd013770ffc0e7b4a6160611c850";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecast);
-}
-function displayForecast(response) {
-  console.log(response.data);
-}
 function getTemperature(response) {
   let tempRound = Math.round(response.data.main.temp);
   let conditionsElement = document.querySelector("#conditions");
@@ -38,7 +29,6 @@ function getTemperature(response) {
   let h4 = document.querySelector("h4");
   h4.innerHTML = `${tempRound}<span class= "units">
             °F</span>`;
-  console.log(response.data);
 
   conditionsElement.innerHTML = `${response.data.weather[0].description}`;
   humidityElement.innerHTML = `Humidity %: ${response.data.main.humidity}`;
